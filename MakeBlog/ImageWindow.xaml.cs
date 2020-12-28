2c17ab16ac03464fa2f699d4a6aa22b8f56c3723
reiuniv <18ec055@ms.dendai.ac.jp>
2020-12-28 21:20:26 +0900

    DataGrid

--- MakeBlog/ImageWindow.xaml.cs
+++ MakeBlog/ImageWindow.xaml.cs
@@ -10,6 +10,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+
+public class ImageData
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
+
+    public bool Send { get; set; }
+}
+
 
 namespace MakeBlog
 {
@@ -22,11 +33,61 @@ namespace MakeBlog
         {
             InitializeComponent();
             MainWindow Mainwin = (MainWindow)Application.Current.MainWindow;
+            var datalist = new ObservableCollection<ImageData>();
+            this.Image_List_Window.ItemsSource = datalist;
+
 
         }
 
+        private void File_Click(object sender, RoutedEventArgs e)
+        {//CSV経由でうまく出来ない？
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            ImageWindow imgwin = new ImageWindow();
+            imgwin.DataContext = this;
+
+
+            // ★★★複数のファイルを選択できるようにするかどうかの設定★★★
+            openFileDialog.Multiselect = true;
+
+            // フィルターの設定
+            openFileDialog.Filter = "画像ファイル|*.bmp;*.gif;*.png;*.jpg;*.jpeg";
+
+            // ダイアログボックスの表示
+            openFileDialog.ShowDialog();
+
+
+            // 選択されたファイルをテキストボックスに表示する
+            foreach (string strFilePath in openFileDialog.FileNames)
+            {
+                // ファイルパスからファイル名を取得
+                string strFileName = System.IO.Path.GetFileName(strFilePath);
+                var datalist = Image_List_Window.ItemsSource as ObservableCollection<ImageData>;
+                var data = new ImageData { Send = false, Name = strFileName, Path = strFilePath};
+                datalist.Add(data);
+
+            }
+        }
+
+
         private void Image_List_Window_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
+        }
+
+        private void List_Delete(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_Send_Click(object sender, RoutedEventArgs e)
+        {
+            var datalist = Image_List_Window.ItemsSource as ObservableCollection<ImageData>;
+
+        }
+
+        private void Image_List_Window_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
 
         }
     }
--- MakeBlog/MainWindow.xaml.cs
+++ MakeBlog/MainWindow.xaml.cs
@@ -20,14 +20,6 @@ using System.Collections.ObjectModel;
 namespace MakeBlog
 {
 
-    // 性別
-
-    // DataGridに表示するデータ
-    public class Person
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -36,8 +28,7 @@ namespace MakeBlog
         public MainWindow()
         {
             InitializeComponent();
-            var datalist = new ObservableCollection<Person>();
-            this.File_List.ItemsSource = datalist;
+
         }
 
         private void Button_Click_Color(object sender, RoutedEventArgs e)
@@ -56,10 +47,6 @@ namespace MakeBlog
         {
             var imgwin = new ImageWindow();
             imgwin.Show();
-            //Debug.Print(File_List.Name);
-            //imgwin.Image_List_Window.Items.Add(File_List.Items);
-            //Debug.Print(File_List.Name);
-            //Debug.Print((string)File_List.SelectedItem);
         }
 
         private void Main_Out_Click(object sender, RoutedEventArgs e)
@@ -71,61 +58,23 @@ namespace MakeBlog
             Main_OutText.Text = v + maintext;
         }
 
-        private void File_Click(object sender, RoutedEventArgs e)
-        {//CSV経由でうまく出来ない？
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            ImageWindow imgwin = new ImageWindow();
-            imgwin.DataContext = this;
-
-
-            // ★★★複数のファイルを選択できるようにするかどうかの設定★★★
-            openFileDialog.Multiselect = true;
-
-            // フィルターの設定
-            openFileDialog.Filter = "画像ファイル|*.bmp;*.gif;*.png;*.jpg;*.jpeg";
-
-            // ダイアログボックスの表示
-            openFileDialog.ShowDialog();
-
-
-                // 選択されたファイルをテキストボックスに表示する
-                foreach (string strFilePath in openFileDialog.FileNames)
-                {
-                    // ファイルパスからファイル名を取得
-                    string strFileName = System.IO.Path.GetFileName(strFilePath);
-                Debug.Print(strFileName);
-                var datalist = File_List.ItemsSource as ObservableCollection<Person>;
-               // var datalist2 = imgwin.Image_List_Window.ItemsSource as ObservableCollection<Person>;
-                var data = new Person { Name = strFileName, Path = strFilePath };
-                datalist.Add(data);
-            }
-
-            //this.File_List.ItemsSource = datalist;
-            //imgwin.Image_List_Window.ItemsSource = datalist;
-        }
+        
 
         private void List_Delete(object sender, RoutedEventArgs e)
         {
-            /// 項目を削除します
-            // 選択項目がない場合は処理をしない
-            if (File_List.SelectedItems.Count == 0)
-                return;
 
-            // 選択された項目を削除
-            File_List.Items.RemoveAt(File_List.SelectedIndex);
         }
 
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        private void File_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
 
-public partial class ImageWindow : Window
-{
-    public ImageWindow()
-    {
-        var window1 = this.DataContext;
-    }
-}
+
